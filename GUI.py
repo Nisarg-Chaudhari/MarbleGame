f608738daf6336f3a173a6a5027b1d907a967360
Nisarg <nisarg5601@gmail.com>
2020-01-15 08:46:48 +0530

    no. of marbles added on top right

--- GUI.py
+++ GUI.py
@@ -261,6 +261,8 @@ while status():
     movelst=[]
 
     board()
+    score = font2.render(str(count()),False,white)
+    win.blit(score,(400,10))
     pygame.display.update()
     (marb_i,marb_j) = user_input()
 
